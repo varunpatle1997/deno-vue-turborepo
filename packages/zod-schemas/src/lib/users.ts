7846cf31ba73e4859c9b4f9055faecde76df746d
Divyanshu Rathore <divyanshu03@live.com>
2025-02-12 19:50:02 +0530

    test pipeline

--- packages/zod-schemas/src/lib/users.ts
+++ packages/zod-schemas/src/lib/users.ts
@@ -2,7 +2,7 @@ import { z } from "zod";
 
 export const userSchema = z
   .object({
-    name: z.string().min(3, "Name should have a minimum of 3 characters."),
+    name: z.string().min(5, "Name should have a minimum of 5 characters."),
     email: z.string().email("Invalid email"),
   })
   .strict();
