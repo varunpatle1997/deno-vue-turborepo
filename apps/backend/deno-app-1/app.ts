f3e4df550af131c46d76fcb17260b21b582f2c94
Divyanshu Rathore <divyanshu03@live.com>
2025-02-28 15:05:38 +0530

    Adds logging of routes

--- apps/backend/deno-app-1/app.ts
+++ apps/backend/deno-app-1/app.ts
@@ -2,14 +2,20 @@ import index from "./src/routes/index.route.ts";
 import createApp from "./src/lib/create-app.ts";
 import configuireOpenAPI from "./src/config/open-api.config.ts";
 import tasks from "./src/routes/tasks/tasks.index.ts";
-const app = createApp();
 
+const app = createApp();
 configuireOpenAPI(app);
 
 const routes = { "/": index, "/tasks": tasks };
 
 Object.entries(routes).forEach(([path, router]) => {
   app.route(path, router);
+});
+
+console.log("\n Final Registered Routes (app.ts): ");
+
+app.routes.forEach(({ method, path }) => {
+  console.log(`${method.toUpperCase()} ${path}`);
 });
 
 export default app;
